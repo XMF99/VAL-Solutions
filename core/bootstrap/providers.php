b326be843b533a35206a511ccc5594a60a296712
Ahmed Alharbi <143522000+XMF99@users.noreply.github.com>
2026-05-08 23:14:39 +0300

    Add WhatsappModuleServiceProvider to providers

--- core/bootstrap/providers.php
+++ core/bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\WhatsappModuleServiceProvider::class,
     App\Providers\BladeDirectivesServiceProvider::class,
     App\Providers\GlobalVariablesServiceProvider::class,
 ];
